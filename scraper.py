3e9b3c8532b084a6ae145126c633f4c48edebe07
k <koolkat@localhost.localdomain>
2015-08-01 16:08:11 +0200

    first commit

---
+++ scraper.py
@@ -0,0 +1,84 @@
+#!/bin/python3
+#
+#         Script to get info from website http://www.notaires.fr
+#
+
+import concurrent.futures
+import io
+import re
+import json
+import time
+from bs4 import BeautifulSoup
+from requests import Session
+
+website = 'http://www.notaires.fr'
+ajax_url = website + '/fr/views/ajax?type=&\
+field_office_department_value=&\
+field_notary_surname_value=&\
+field_jv_adresses_line_6city_value=&\
+field_jv_adresses_line_6cp_value=&\
+departement1=&\
+field_notary_langueparlee_taxo_tid=All'
+save_file = open("data/just_links",'w+')
+ajaxfile = open('data/xmlhhttprequest', 'r')
+payload_part1 = ajaxfile.readline().rstrip('\n')
+payload_part2 = ajaxfile.readline().rstrip('\n')
+hdrs = {
+    # 'Host': 'www.notaires.fr',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:39.0) Gecko/20100101 Firefox/39.0',
+    # 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    # 'Accept-Language': 'en-US,en;q=0.5',
+    # 'Accept-Encoding': 'gzip, deflate',
+    'Connection': 'keep-alive',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'X-Requested-With': 'XMLHttpRequest',
+}
+
+def getPayload(page_number):
+    payload = payload_part1+str(page_number)+payload_part2
+    return payload
+
+def getPage(payload, s):
+    r  = s.post(ajax_url, data=payload, headers=hdrs)
+    if r.status_code != 200:
+        save_file.write('\n'.join(exp.findall(pages.getvalue()))+'\n')
+        print("Status code not 200 number of pages %",page_number)
+        return r.status_code
+    return r.json()[1]['data']
+
+def makeRequests(pages):
+    s = Session()
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=20)
+    future_to_page = {executor.submit(getPage, getPayload(page_number), s):
+                      page_number for page_number in range(1,977)}
+    for future in concurrent.futures.as_completed(future_to_page):
+        page = future_to_page[future]
+        try:
+            data = future.result()
+            pages.write(data)
+        except Exception as exc:
+            print('Page %i generated an exception: %s' % (page,exc))
+            return
+        else:
+            print('Got page %i' % page)
+
+def getProfileLinks():
+    pages = io.StringIO()
+    page_number = 1
+    exp = re.compile('<a class="btn btn-actions mq-hos" href="?\'?([^"\'>]*)')
+    makeRequests(pages)
+    links = exp.findall(pages.getvalue())
+    pages.close()
+    save_file.write('\n'.join(links)+'\n')
+    save_file.close()
+    return links
+
+def main():
+    links = getProfileLinks()
+    #f = open('data/just_links','r')
+    #links = [line.strip('\n') for line in f.readlines()]
+    print(links)
+
+    
+if __name__ == "__main__":
+    main()
